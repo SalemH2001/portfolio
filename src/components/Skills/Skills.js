1e2c51fcc62df4ca026596e9b4ba3c794a27c246
unknown <hatoumsalem@gmail.com>
2023-01-31 18:22:13 +0200

    first commmit

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,72 +1,81 @@
 import React, { useState, useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
+import { motion, AnimatePresence } from 'framer-motion';
+
 import { FaHtml5, FaCss3, FaJs, FaReact, FaPhp, FaLaravel, FaNodeJs } from 'react-icons/fa';
-import Fade from 'react-reveal/Fade';
 
 const Skills = () => {
-    const [animation, setAnimation] = useState(false);
     const [ref, inView] = useInView({
         threshold: 0.5,
         triggerOnce: false,
     });
 
-    useEffect(() => {
-        setAnimation(inView);
-    }, [inView]);
+    const gridVariants = {
+        hidden: {
+            opacity: 0
+        },
+        visible: {
+            opacity: 1,
+            transition: {
+                delay: 0.5,
+                duration: 1
+            }
+        }
+    };
 
     return (
         <div style={{ width: "100%", maxWidth: "1600px", overflowX: "hidden" }}>
             <section className="bg-primary text-white px-5 py-32" id="skills" ref={ref}>
-            <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[180px] border-indigo-600 pb-2">
-            Skills
-          </h2>
-                <ul className="skills-list mx-auto" style={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(120px, auto))", gridGap: "10px", margin: "1rem auto" }}>
-                    <Fade bottom when={animation}>
-                        <li className="skill mb-4 " >
-                            <FaHtml5 className=" h-8 w-8 mr-2" />
-                            <span style={{ fontSize: "2vw" }}>HTML</span>
-                        </li>
-                    </Fade>
-                    <Fade bottom when={animation}>
-                        <li className="skill mb-4" >
-                            <FaCss3 className="h-8 w-8 mr-2" />
-                            <span style={{ fontSize: "2vw" }}>CSS</span>
-                        </li>
-                    </Fade>
-                    <Fade bottom when={animation}>
-                        <li className="skill mb-4" >
-                            <FaJs className="h-8 w-8 mr-2" />
-                            <span style={{ fontSize: "2vw" }}>JavaScript</span>
-                        </li>
-                    </Fade>
-                    <Fade bottom when={animation}>
-                        <li className="skill mb-4" >
-                            <FaReact className="h-8 w-8 mr-2" />
-                            <span style={{ fontSize: "2vw" }}>React</span>
-                        </li>
-                    </Fade>
-                    <Fade bottom when={animation}>
-                        <li className="skill mb-4" >
-                            <FaPhp className="h-8 w-8 mr-2" />
-                            <span style={{ fontSize: "2vw" }}>PHP</span>
-                        </li>
-                    </Fade>
-                    <Fade bottom when={animation}>
-                        <li className="skill mb-4" >
-                            <FaLaravel className="h-8 w-8 mr-2" />
-                            <span style={{ fontSize: "2vw" }}>Laravel</span>
-                        </li>
-                    </Fade>
-                    <Fade bottom when={animation}>
-                        <li className="skill mb-4" >
-                            <FaNodeJs className="h-8 w-8 mr-2" />
-                            <span style={{ fontSize: "2vw" }}>Node.js</span>
-                        </li>
-                    </Fade>
-                </ul>
+                <h2 className="text-center text-3xl font-bold mb-4">Skills</h2>
+                <AnimatePresence>
+                    {inView && (
+                        <motion.div
+                            initial="hidden"
+                            animate="visible"
+                            exit="hidden"
+                            variants={gridVariants}
+                            style={{
+                                display: 'grid',
+                                gridTemplateColumns: 'repeat(auto-fit, minmax(150px, 1fr))',
+                                gridGap: '20px',
+                                justifyItems: 'center',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <div className="mb-2 text-center">
+                                <FaHtml5 className="text-5xl mr-4" />
+                                <p>HTML5</p>
+                            </div>
+                            <div className="mb-2 text-center">
+                                <FaCss3 className="text-5xl mr-4" />
+                                <p>CSS3</p>
+                            </div>
+                            <div className="mb-2 text-center">
+                                <FaJs className="text-5xl mr-4" />
+                                <p>JavaScript</p>
+                            </div>
+                            <div className="mb-2 text-center">
+                                <FaReact className="text-5xl mr-4" />
+                                <p>React</p>
+                            </div>
+                            <div className="mb-2 text-center">
+                                <FaPhp className="text-5xl mr-4" />
+                                <p>PHP</p>
+                            </div>
+                            <div className="mb-2 text-center">
+                                <FaLaravel className="text-5xl mr-4" />
+                                <p>Laravel</p>
+                            </div>
+                            <div className="mb-2 text-center">
+                                <FaNodeJs className="text-5xl mr-4" />
+                                <p>Node.js</p>
+                            </div>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
             </section>
         </div>
     );
 };
-export default Skills;
 
+export default Skills;
